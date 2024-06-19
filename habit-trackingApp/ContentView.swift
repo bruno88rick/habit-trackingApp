fe3a0225aeeff246d0c87777ff8f79bcdc8adb1d
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-06-19 16:36:23 -0300

    Initial Commit

---
+++ habit-trackingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  habit-trackingApp
+//
+//  Created by Bruno Oliveira on 19/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ habit-trackingApp/habit_trackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackingAppApp.swift
+//  habit-trackingApp
+//
+//  Created by Bruno Oliveira on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
